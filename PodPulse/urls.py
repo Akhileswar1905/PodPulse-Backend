0fde5f92ace31cac3bdae7fdf7a8391dc5b5803b
KalaHanok <hanokkala123@gmail.com>
2023-04-24 13:37:55 +0530

    added  commit to use api routes

--- PodPulse/urls.py
+++ PodPulse/urls.py
@@ -23,13 +23,25 @@ from knox import views as knox_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/register/', views.RegisterAPI.as_view(), name='register'),
-    path('api/login/', views.LoginAPI.as_view(), name='login'),
+    path('api/login/', views.LoginAPI.as_view(), name='login'),#normal login
     path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
     path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
     re_path(r'^auth/', include('drf_social_oauth2.urls', namespace='drf')),
-    path('api/addAudio/',views.AddAudioFiles.as_view()),
-    path('api/addVideo/',views.AddVideoFiles.as_view()),
-    path('api/audiopodcasts/',views.GetAudioFiles.as_view()),
-    path('api/audiofavourates/',views.AddUserFavouratesAudios.as_view()),
-    path('api/favourateaudios/',views.GetFavourateAudios.as_view())
+    path('api/addAudio/',views.AddAudioFiles.as_view()),#to add audio podcasts
+    path('api/addVideo/',views.AddVideoFiles.as_view()),#to add vedio podcasts
+    path('api/audiopodcasts/',views.GetAudioFiles.as_view()),#to retrive audio podcasts based on rating sorted already so,no need to sort again
+    path('api/videopodcasts/',views.GetVideoFiles.as_view()),# same as above but for video podcasts
+    path('api/addaudiofavourates/',views.AddUserFavouratesAudios.as_view()),#add a audio podcast into favourate
+    path('api/addvideofavourates/',views.AddUserFavouratesAudios.as_view()),#add a video podcast into favourate
+    path('api/favourateaudios/',views.GetFavourateAudios.as_view()),#to retrive favourate audio podcasts
+    path('api/favouratevideos/',views.GetFavourateVideos.as_view()),#to retrive favourate vedio podcasts
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+#Note to access the audio and video file after making the api request
+#you basically get the path of the file 
+#so you need to make another 'GET' request as shown below
+#http://127.0.0.1:8000/media/<the path of the file that you received in the request>
+#also note that every time you make a request you need to send  access token
+#which you get as response when you login 
+#in headers you need to send it as shown below
+# Authorization : Token <token>
